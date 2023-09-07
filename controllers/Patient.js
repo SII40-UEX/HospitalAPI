d9fffd629b60b07e37c4b55794fac8924e593244
slasom <slasom@unex.es>
2023-09-07 20:49:51 +0200

    First commit

---
+++ controllers/Patient.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Patient = require('../service/PatientService');
+
+module.exports.getPatient = function getPatient (req, res, next, id) {
+  Patient.getPatient(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.updatePatient = function updatePatient (req, res, next, id) {
+  Patient.updatePatient(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ service/PatientService.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+/**
+ * Gets the history record
+ *
+ * id Long ID of pet to return
+ * returns String
+ **/
+exports.getPatient = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "Get Correctly";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Updates the history record
+ *
+ * id Long ID of pet to return
+ * returns String
+ **/
+exports.updatePatient = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "Get Correctly";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
