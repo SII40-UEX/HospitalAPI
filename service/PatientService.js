1d942c17e31844ff9f0e9c5464668b6aea61b2e7
Sergio <32648184+slasom@users.noreply.github.com>
2024-06-22 10:35:47 +0000

    updated patient endpoint

--- service/PatientService.js
+++ service/PatientService.js
@@ -10,7 +10,7 @@
 exports.getPatient = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "Get Correctly";
+    examples['application/json'] = "Get Correctly + id";
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
